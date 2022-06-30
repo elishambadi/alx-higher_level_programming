1148e415d825b62edcd39923f67fec320f51c6de
Elisha Mbadi <embadi43@gmail.com>
2022-06-30 17:09:36 +0300

    add task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-import 0-import_add.py
-a = 1
-b = 2
-result = add(a, b)
-print("{:d} + {:d} = {:d}".format(a, b, result))
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    result = add(a, b)
+    print("{:d} + {:d} = {:d}".format(a, b, result))
