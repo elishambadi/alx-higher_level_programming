6ed73abcf3713af095827d3dd8f4b0270ee116da
Elisha Mbadi <embadi43@gmail.com>
2022-07-04 21:19:36 +0300

    add task 4

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    cp_list = []
+    cp_list = my_list.copy()
+    if idx < 0:
+        return cp_list
+    elif idx > len(my_list):
+        return cp_list
+
+    cp_list[idx] = element
+    return cp_list
