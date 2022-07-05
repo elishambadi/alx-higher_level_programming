e772178773adc1b040679dd65a8002dd750d90cf
Elisha Mbadi <embadi43@gmail.com>
2022-07-05 15:11:54 +0300

    add = check

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -4,7 +4,7 @@ def new_in_list(my_list, idx, element):
     cp_list = my_list.copy()
     if idx < 0:
         return cp_list
-    elif idx > len(my_list):
+    elif idx >= len(my_list):
         return cp_list
 
     cp_list[idx] = element
