823fd70e1b202b6bb681bc48f1880ec185627c46
Elisha Mbadi <embadi43@gmail.com>
2022-08-02 21:08:34 +0300

    add task 2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    cnt = 0
+
+    for i in range(x):
+        try:
+            try:
+                print("{:d}".format(my_list[i]), end="")
+                cnt += 1
+            except (ValueError, TypeError):
+                pass
+        except IndexError:
+            raise
+    print("")
+    return cnt
