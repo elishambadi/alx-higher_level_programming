dd432146742bb3af0f5d92e2622ffdfbecd170e2
Elisha Mbadi <embadi43@gmail.com>
2022-08-04 20:37:17 +0300

    make initialized public

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -9,8 +9,8 @@ class Rectangle:
     '''
 
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
@@ -33,7 +33,7 @@ class Rectangle:
     def height(self, value):
         if type(value) != int:
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
         else:
             self.__height = value
---
+++ 0x08-python-more_classes/main_8.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+Rectangle = __import__('1-rectangle').Rectangle
+
+try:
+    print("Trying")
+    my_rectangle = Rectangle(2, -3)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+try:
+    my_rectangle = Rectangle(-2, 3)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
