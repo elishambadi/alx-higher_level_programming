483abeb5bbc2bc5bdfdde7ec7caa536f6aec241f
Elisha Mbadi <embadi43@gmail.com>
2022-08-04 08:35:52 +0300

    add task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''Define size of square
+'''
+
+
+class Square:
+    '''square(size)
+       Defines a square with it's size
+    '''
+    __size = 0
+
+    def __init__(self, size):
+        self.__size = size
