1ab006ca57cd411a454e9683451737424221ef39
Elisha Mbadi <embadi43@gmail.com>
2022-08-04 08:41:47 +0300

    add task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''Define size of square
+'''
+
+
+class Square:
+    '''square(size)
+       Defines a square with it's size
+    '''
+    __size = 0
+
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size myst be >= 0")
+        else:
+            self.__size = size
