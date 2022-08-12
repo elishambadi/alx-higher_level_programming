ab67f6b33b0988dfe8f46f49e570a693ac7f8a7e
Elisha Mbadi <embadi43@gmail.com>
2022-08-12 13:05:39 +0300

    add func docs

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -3,6 +3,9 @@
    Module that has dir method
 '''
 def lookup(obj):
+    '''
+       lookup(object).  Shows methods in an object 
+    '''
     methods = []
     for dirs in dir(obj):
         methods.append(dirs)
