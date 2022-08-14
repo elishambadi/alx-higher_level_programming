830b811f2ccb0acda1dc37748ba41eb4abb8453d
Elisha Mbadi <embadi43@gmail.com>
2022-08-14 14:57:00 +0300

    add task 2

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+   Handling text files
+'''
+
+
+def append_write(filename="", text=""):
+    '''
+       append_write(filename, text)
+       Adds text to a textfile
+    '''
+
+    with open(filename, mode='a', encoding='utf-8') as f:
+        return f.write(text)	
