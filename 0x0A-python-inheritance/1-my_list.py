18f2cfa423f0c61e9ff3603c0da29bd8fbb6f84b
Elisha Mbadi <embadi43@gmail.com>
2022-08-14 14:39:07 +0300

    add readme

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,17 +1,28 @@
 #!/usr/bin/python3
+'''
+   List Module
+'''
+
+
 class MyList(list):
     pass
+
+
+'''
+   Print sorted list. print_sorted()
+'''
+
 
 def print_sorted(self):
     bubblesort(MyList)
     print(MyList)
 
-def bubblesort(list):
 
-# Swap the elements to arrange in order
-   for iter_num in range(len(list)-1,0,-1):
-      for idx in range(iter_num):
-         if list[idx]>list[idx+1]:
-            temp = list[idx]
-            list[idx] = list[idx+1]
-            list[idx+1] = temp
+def bubblesort(list):
+    # Swap the elements to arrange in order
+    for iter_num in range(len(list)-1, 0, -1):
+        for idx in range(iter_num):
+            if list[idx] > list[idx + 1]:
+                temp = list[idx]
+                list[idx] = list[idx + 1]
+                list[idx+1] = temp
---
+++ 0x0B-python-input_output/README.md
@@ -0,0 +1,5 @@
+#Handling files
+
+Usage of with ... as ...
+
+JSON encoding and decoding of objects
