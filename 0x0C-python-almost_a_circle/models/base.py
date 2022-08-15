b2825eda22fd428d100285c3c81e16ccefcfa6d7
Elisha Mbadi <embadi43@gmail.com>
2022-08-15 14:30:01 +0300

    add base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+'''
+   Base Class for models
+'''
+
+
+class Base:
+    __nb_objects = 0
+    id = 1
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            self.id = Base.id
+            Base.id += 1
+        Base.__nb_objects += 1
+
+    @property
+    def nb_obj(self):
+        return self.__nb_objects
