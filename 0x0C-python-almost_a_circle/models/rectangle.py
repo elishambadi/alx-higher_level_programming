4679a3e5dd065a1af23fe32316ba934bc6a374bd
Elisha Mbadi <embadi43@gmail.com>
2022-08-16 14:05:49 +0300

    add x , y to display

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -81,8 +81,11 @@ class Rectangle(Base):
         '''
            display() - Displays the rectangle
         '''
+        for i in range(self.__y):
+            print('')
+
         for i in range(self.__height):
-            print("#" * self.__width)
+            print(' ' * self.__x + "#" * self.__width)
 
     def __str__(self):
         '''
