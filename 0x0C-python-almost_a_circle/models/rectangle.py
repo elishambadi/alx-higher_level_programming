51815bbb75b5938ad41518d60e6793f2816b7a30
Elisha Mbadi <embadi43@gmail.com>
2022-08-16 13:10:42 +0300

    add __str__ method

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -83,3 +83,8 @@ class Rectangle(Base):
         '''
         for i in range(self.__height):
             print("#" * self.__width)
+
+    def __str__(self):
+        return "[{}] ({}) {}/{} - {}/{}"
+        .format(__class__.__name__, self.id, self.__x, self.__y,
+                self.__width, self.__height)
