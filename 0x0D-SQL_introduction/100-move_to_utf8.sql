53c63ed9679874a9626846615fee59a53a06915e
Elisha Mbadi <embadi43@gmail.com>
2022-09-06 15:20:04 +0300

    edit encoding

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,5 @@
 -- Move DB, table and field to UTF8
 
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-ALTER TABLE second_table CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-ALTER TABLE second_table MODIFY name VARCHAR(256) CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE second_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE second_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
