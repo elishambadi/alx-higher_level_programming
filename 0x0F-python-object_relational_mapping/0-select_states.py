cefeee86c03c1dc9cc950261cbb20a64a94cc7ab
elishambadi <embadi43@gmail.com>
2022-10-14 08:50:27 -0400

    final fixes

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,28 +7,28 @@
 
 import sqlalchemy
 import MySQLdb
-
-MY_HOST = 'localhost'
-MY_USER = 'root'
-MY_PASS = '##Spidey43##'
-MY_DB = 'hbtn_0e_0_usa'
-
-db = MySQLdb.connect(host=MY_HOST, user=MY_USER, passwd=MY_PASS, db=MY_DB)
-cur = db.cursor()
-states = cur.execute("SELECT * FROM states")
-print("Selected %s rows" % states)
-
-rows = cur.fetchall()
-for row in rows:
-    print("(", end="")
-    count = 0
-    for col in row:
-        if count != 1:
-            print("%s, " % col, end="")
-        else:
-            print("'%s'" % col, end="")
-        count = count + 1
-    print(")")
+import sys
 
 if __name__ == "__main__":
-    print("Testing...")
+    MY_USER = sys.argv[1]
+    MY_PASS = sys.argv[2]
+    MY_DB = sys.argv[3]
+
+    db = MySQLdb.connect(host='localhost',
+                         user=MY_USER,
+                         passwd=MY_PASS,
+                         db=MY_DB)
+    cur = db.cursor()
+    states = cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+
+    rows = cur.fetchall()
+    for row in rows:
+        print("(", end="")
+        count = 0
+        for col in row:
+            if count != 1:
+                print("%s, " % col, end="")
+            else:
+                print("'%s'" % col, end="")
+            count = count + 1
+        print(")")
