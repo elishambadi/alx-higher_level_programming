6fc5ed346f9226579bab993a5c607f1da3debcba
elishambadi <embadi43@gmail.com>
2022-10-14 08:54:30 -0400

    add task

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+
+"""
+    A script that filters states by name
+"""
+
+import sqlalchemy
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    MY_USER = sys.argv[1]
+    MY_PASS = sys.argv[2]
+    MY_DB = sys.argv[3]
+
+    db = MySQLdb.connect(host='localhost',
+                         user=MY_USER,
+                         passwd=MY_PASS,
+                         db=MY_DB)
+    cur = db.cursor()
+    states = cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC")
+
+    rows = cur.fetchall()
+    for row in rows:
+        print("(", end="")
+        count = 0
+        for col in row:
+            if count != 1:
+                print("%s, " % col, end="")
+            else:
+                print("'%s'" % col, end="")
+            count = count + 1
+        print(")")
