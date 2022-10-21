41cbf7d8e54bd3600149046773c6593d922b40d9
elishambadi <embadi43@gmail.com>
2022-10-21 07:34:58 +0300

    change header

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # Using cURL to show response size in bytes
 curl -s -o /dev/null -X GET google.com -D file.txt; cat file.txt | grep Content-Length | sed 's/Content-Length: //g';
