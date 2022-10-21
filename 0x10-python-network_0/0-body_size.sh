c48d802c06933113201036d522b57049be0debed
elishambadi <embadi43@gmail.com>
2022-10-21 07:36:16 +0300

    add user input

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Using cURL to show response size in bytes
-curl -s -o /dev/null -X GET google.com -D file.txt; cat file.txt | grep Content-Length | sed 's/Content-Length: //g';
+curl -s -o /dev/null -X GET $1 -D file.txt; cat file.txt | grep Content-Length | sed 's/Content-Length: //g';
