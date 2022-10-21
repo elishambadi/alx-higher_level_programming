e370d030eae9330a007ef581bc8c8a68c03383ae
elishambadi <embadi43@gmail.com>
2022-10-21 07:34:08 +0300

    add task 1

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-# Using cURL to show response size in ytes
-curl -
+# Using cURL to show response size in bytes
+curl -s -o /dev/null -X GET google.com -D file.txt; cat file.txt | grep Content-Length | sed 's/Content-Length: //g';
