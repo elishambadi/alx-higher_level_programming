80989df3c4fcdb58b42678f8bbb625716c963a70
elishambadi <embadi43@gmail.com>
2022-10-23 10:59:16 +0300

    add task 5

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+   Send request using requests package, extract a header value
+"""
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    url = sys.argv[1]
+
+    r = requests.get(url)
+    print(r.header[''X-Request-Id])
