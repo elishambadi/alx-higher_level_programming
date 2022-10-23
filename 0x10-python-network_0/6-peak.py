261d8407e4107451e61c5001244bf46fd9699e51
elishambadi <embadi43@gmail.com>
2022-10-23 06:01:35 +0300

    add task 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,22 @@
+#!/bin/python3
+
+"""
+   Finds the peak in a list of numbers
+"""
+
+
+def find_peak(list_of_integers):
+    size = len(list_of_integers)
+    list = list_of_integers
+    i = 1
+
+    if len(list) == 0:
+        return None
+
+    peak = list_of_integers[0]
+    for i in range(size-1):
+        if list[i-1] < list[i] and list[i] > list[i+1]:
+            peak = list[i]
+        else:
+            pass
+    return peak
