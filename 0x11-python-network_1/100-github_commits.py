2455adc5387b7c1e08db1e72c3119f335ab370c5
elishambadi <embadi43@gmail.com>
2022-10-24 11:12:38 +0300

    add adv git task

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""
+   Send requests to Github API; returns last 10 commits
+   Arguments:
+      r_name: Repository Name
+      o_name: Owner Name
+"""
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    r_name = sys.argv[1]
+    o_name = sys.argv[2]
+
+    # Auth section
+    token = "Bearer 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"
+    headers = {'Accept': 'application/vnd.github+json', 'Authorization': token}
+
+    url = "https://api.github.com/repos/"+o_name+"/"+r_name+"/commits"
+    params = {'per_page': 10}
+
+    res = requests.get(url, params=params, headers=headers)
+    json = res.json()
+
+    for i in range(10):
+        print("{}: {}".format(json[i]["sha"], json[i]["commit"]["author"]["name"]))
+
