32c0d0b80980a1dbcb483e86a1eb24f5a5418bf4
elishambadi <embadi43@gmail.com>
2022-10-24 10:51:51 +0300

    use github api

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""
+   Send requests to Github API; returns ID
+   Arguments:
+      uname: Username
+      pass_:  Password
+"""
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    uname = sys.argv[1]
+    pass_ = sys.argv[2]
+
+    auth = "Bearer "+pass_
+    payload = {'username':uname}
+    headers = {'Accept':'application/vnd.github+json', 'Authorization': auth}
+
+    url = "https://api.github.com/users/"+uname
+
+    res = requests.get(url, params=payload, headers=headers)
+    if res.status_code == 200:
+        print(res.json()["id"])
+    else:
+        print("None")
