296dc596e963d2ba5100b63d59555dbfae8d8d81
elishambadi <embadi43@gmail.com>
2022-11-09 14:58:40 +0300

    add NaN check

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-for (let x = 0; x < process.argv[2]; x++) {
+for (let x = 0; x < parseInt(process.argv[2]); x++) {
   console.log('C is fun');
 }
-if (process.argv.length === 2) {
+if (process.argv.length === 2 || Number.isNaN(parseInt(process.argv[2]))) {
   console.log('Missing number of occurrences');
 }
