9c2111a70135b057c2e964de0fcb7d418413bf40
elishambadi <embadi43@gmail.com>
2022-11-26 20:53:06 +0300

    add task 5

--- 0x15-javascript-web_jquery/4-script.js
+++ 0x15-javascript-web_jquery/4-script.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   $('DIV#red_header').attr('class', 'red');
-  $('#red_header').click(function () {
+  $('DIV#red_header').click(function () {
     // console.log('Red header clicked');
     $('DIV#red_header').toggleClass('green');
   });
