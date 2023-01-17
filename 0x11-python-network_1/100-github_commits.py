44101446afc4b606d5ff4ee07357ae174749f7db
elishambadi <embadi43@gmail.com>
2023-01-17 17:02:50 +0300

    fix task 11

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,27 +1,20 @@
 #!/usr/bin/python3
+"""lists the 10 most recent commits on a git repo
+"""
+import sys
+import requests
 
-"""
-   Send requests to Github API; returns 10 latest commits
-   Arguments:
-      r_name: Repository name
-      o_name: Owner name
-"""
 
 if __name__ == "__main__":
-    import requests
-    import sys
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
 
-    r_name = sys.argv[1]
-    o_name = sys.argv[2]
-
-    auth = "Bearer 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"
-    headers = {'Accept': 'application/vnd.github+json', 'Authorization': auth}
-
-    url = "https://api.github.com/repos/"+o_name+"/"+r_name+"/commits"
-
-    res = requests.get(url, headers=headers)
-    res = res.json()
-
-    for i in range(10):
-        print("{}: {}".format(res[i]["sha"],
-                              res[i]["commit"]["author"]["name"]))
+    r = requests.get(url)
+    commits = r.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
