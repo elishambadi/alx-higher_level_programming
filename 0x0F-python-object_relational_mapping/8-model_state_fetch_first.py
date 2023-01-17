1d1022ded894da2ba5152de187402eb4e04e2613
elishambadi <embadi43@gmail.com>
2023-01-17 16:32:27 +0300

    add task 8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,30 +1,25 @@
 #!/usr/bin/python3
 
-
 """
     Find cities using SQL Alchemy
 """
 
-import sqlalchemy
-import MySQLdb
+
 import sys
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_state import State
+
 
 if __name__ == "__main__":
-    from model_state import Base, State
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2],
-                                   sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-    # All classes tied to sqlalchemy imported now. Let's use them!
-
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    count = 0
-    for state in session.query(State).order_by(State.id):
-        if count == 0:
-            print("{}: {}".format(state.id, state.name))
-        else:
-            exit(0)
-        count = count + 1
+
+    state = session.query(State).order_by(State.id).first()
+    if state is None:
+        print("Nothing")
+    else:
+        print("{}: {}".format(state.id, state.name))
